fa5ab6401068ba7d2e158afe34c25d5b0d930cca
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2021-02-13 16:25:54 -0300

    Added validation to authentication route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,19 @@
+import { celebrate, Joi } from 'celebrate';
 import { Router } from 'express';
 import AuthController from '../controllers/AuthController';
 
 const authRouter = Router();
 const authController = new AuthController();
 
-authRouter.post('/', authController.create);
+authRouter.post(
+  '/',
+  celebrate({
+    body: {
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  authController.create
+);
 
 export default authRouter;
