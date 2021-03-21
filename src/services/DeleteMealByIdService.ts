eb84d9737b5990fa9006211b3df8f939f8cb62a3
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2021-03-21 18:43:52 -0300

    Now returning total nutrients of the meal when deleting

--- src/services/DeleteMealByIdService.ts
+++ src/services/DeleteMealByIdService.ts
@@ -1,21 +1,58 @@
+import IFoodsRepository from '../database/repositories/base/IFoodsRepository';
+import IMealsFoodsRepository from '../database/repositories/base/IMealsFoodsRepository';
 import IMealsRepository from '../database/repositories/base/IMealsRepository';
 import AppError from '../errors/AppError';
 import Meal from '../models/Meal';
+import MealFoods from '../models/MealFoods';
 
 interface IRequest {
   mealId: string;
   userId: string;
 }
 
+interface IResponse extends Meal {
+  energy_kcal: number;
+  energy_kj: number;
+  carbs: number;
+  proteins: number;
+  fats: number;
+}
+
 export default class DeleteMealByIdService {
   private mealsRepository: IMealsRepository;
+  private mealFoodsRepository: IMealsFoodsRepository;
 
-  constructor(mealsRepository: IMealsRepository) {
+  constructor(
+    mealsRepository: IMealsRepository,
+    mealFoodsRepository: IMealsFoodsRepository
+  ) {
     this.mealsRepository = mealsRepository;
+    this.mealFoodsRepository = mealFoodsRepository;
   }
 
-  public async execute({ mealId, userId }: IRequest): Promise<Meal> {
+  public async execute({ mealId, userId }: IRequest): Promise<IResponse> {
     const mealFound = await this.mealsRepository.findById(mealId);
+
+    const foods =
+      (await this.mealFoodsRepository.listByMealId(mealId)) ||
+      ([] as MealFoods[]);
+
+    const nutrients = {
+      energy_kcal: 0,
+      energy_kj: 0,
+      carbs: 0,
+      proteins: 0,
+      fats: 0,
+    };
+
+    foods.length > 0 &&
+      foods.forEach((food) => {
+        nutrients.energy_kcal += food.energy_kcal;
+        nutrients.energy_kj += food.energy_kj;
+        nutrients.carbs += food.carbs;
+        nutrients.proteins += food.proteins;
+        nutrients.fats += food.fats;
+      });
 
     if (!mealFound) {
       throw new AppError('Meals does not exist', 400);
@@ -27,6 +64,6 @@ export default class DeleteMealByIdService {
 
     await this.mealsRepository.deleteById(mealId);
 
-    return mealFound;
+    return { ...mealFound, ...nutrients };
   }
 }
--- src/services/tests/DeleteMealByIdService.spec.ts
+++ src/services/tests/DeleteMealByIdService.spec.ts
@@ -1,33 +1,72 @@
 import DeleteMealByIdService from '../DeleteMealByIdService';
 import FakeMealsRepository from '../../database/repositories/fakes/FakeMealsRepository';
+import FakeMealsFoodsRepository from '../../database/repositories/fakes/FakeMealsFoodsRepository';
 import AppError from '../../errors/AppError';
 
 let fakeMealsRepository: FakeMealsRepository;
+let fakeMealsFoodsRepository: FakeMealsFoodsRepository;
 let deleteMealById: DeleteMealByIdService;
 
 describe('Delete meal by id', () => {
   beforeEach(() => {
     fakeMealsRepository = new FakeMealsRepository();
-    deleteMealById = new DeleteMealByIdService(fakeMealsRepository);
+    fakeMealsFoodsRepository = new FakeMealsFoodsRepository();
+    deleteMealById = new DeleteMealByIdService(
+      fakeMealsRepository,
+      fakeMealsFoodsRepository
+    );
   });
 
   it('should be able do delete a meal', async () => {
     const mealToBeCreated1 = {
-      userId: 'fake-user-id-one',
+      userId: 'fake-user-id-1',
       date: new Date(2021, 0, 20),
-      name: 'fake-meal-one',
+      name: 'fake-meal-1',
     };
 
     const mealToBeCreated2 = {
-      userId: 'fake-user-id-two',
+      userId: 'fake-user-id-2',
       date: new Date(2021, 0, 22),
-      name: 'fake-meal-two',
+      name: 'fake-meal-2',
+    };
+
+    const mealToBeCreated3 = {
+      userId: 'fake-user-id-3',
+      date: new Date(2021, 0, 22),
+      name: 'fake-meal-3',
+    };
+
+    const mealFoodData = {
+      foodId: 'dbd4888b-08b2-4656-8b27-4cb6c289252e',
+      name: 'Abacate',
+      quantity: 100,
+      quantity_unit: 'g',
+      energy_kcal: 96,
+      energy_kj: 402,
+      carbs: 6,
+      proteins: 1.2,
+      fats: 8.4,
     };
 
     const meal1 = await fakeMealsRepository.create(mealToBeCreated1);
+    await fakeMealsFoodsRepository.create({
+      mealId: meal1.id,
+      ...mealFoodData,
+    });
+
     const meal2 = await fakeMealsRepository.create(mealToBeCreated2);
 
-    await deleteMealById.execute({ mealId: meal1.id, userId: meal1.user_id });
+    const meal3 = await fakeMealsRepository.create(mealToBeCreated3);
+
+    await deleteMealById.execute({
+      mealId: meal1.id,
+      userId: meal1.user_id,
+    });
+
+    await deleteMealById.execute({
+      mealId: meal2.id,
+      userId: meal2.user_id,
+    });
 
     const meal1Found = await fakeMealsRepository.findById(meal1.id);
 
@@ -35,7 +74,11 @@ describe('Delete meal by id', () => {
 
     const meal2Found = await fakeMealsRepository.findById(meal2.id);
 
-    expect(meal2Found).toBeTruthy();
+    expect(meal2Found).toBeFalsy();
+
+    const meal3Found = await fakeMealsRepository.findById(meal3.id);
+
+    expect(meal3Found).toBeTruthy();
   });
 
   it('should not be able to delete a meal of another user', async () => {
